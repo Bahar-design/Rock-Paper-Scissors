a01c3cc536999ea2170436d6f60702838593a840
Bahar Abdi <bahar.abdi04@gmail.com>
2024-06-25 16:26:45 -0500

    Add increments and show current scores for all different outcomes

--- script.js
+++ script.js
@@ -27,32 +27,31 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'paper') {
-        alert("Oh no, you lost this round... Paper covers Rock")
-        // increment computer score. show current scores
+        computerScore++
+        alert(`Oh no, you lost this round... Paper covers Rock\nComputer: ${computerScore} You: ${humanScore}`);
     }
     else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        alert("Yay! Rock beats Scissors!")
-        // increment human score. show current scores
+        humanScore++
+        alert(`Yay! Rock beats Scissors!\nComputer: ${computerScore} You: ${humanScore}`)
     }
     else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        alert("Yay! Paper beats Rock!")
-        // increment human score. show current scores
+        humanScore++
+        alert(`Yay! Paper beats Rock!\nComputer: ${computerScore} You: ${humanScore}`)
     }
     else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        alert("Oh no, you lost this round... Scissors cut paper")
-        //increment computer score. show current scores
+        computerScore++
+        alert(`Oh no, you lost this round... Scissors cut paper\nComputer: ${computerScore} You: ${humanScore}`)
     }
     else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        alert("Oh no, you lost this round... Rock crushes Scissors")
-        //increment computer score. show scores
+        computerScore++
+        alert(`Oh no, you lost this round... Rock crushes Scissors\nComputer: ${computerScore} You: ${humanScore}`)
     }
     else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        alert("Yay! Scissors beats Paper!")
-        //increment human score. show scores
+        humanScore++
+        alert(`Yay! Scissors beats Paper!\nComputer: ${computerScore} You: ${humanScore}`)
     }
     else {
-        alert("It's a draw!")
-        //just show scores
+        alert(`It's a draw!\nComputer: ${computerScore} You: ${humanScore}`)
     }
 
     const humanSelection = getHumanChoice();
