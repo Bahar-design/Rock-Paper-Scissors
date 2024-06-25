3b339d4739ac682e511a51deb0f44f2075e3636d
Bahar Abdi <bahar.abdi04@gmail.com>
2024-06-25 16:19:32 -0500

    Create logic for the first round, with if-else on different outcomes

--- script.js
+++ script.js
@@ -24,3 +24,40 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock' && computerChoice === 'paper') {
+        alert("Oh no, you lost this round... Paper covers Rock")
+        // increment computer score. show current scores
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        alert("Yay! Rock beats Scissors!")
+        // increment human score. show current scores
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        alert("Yay! Paper beats Rock!")
+        // increment human score. show current scores
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        alert("Oh no, you lost this round... Scissors cut paper")
+        //increment computer score. show current scores
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        alert("Oh no, you lost this round... Rock crushes Scissors")
+        //increment computer score. show scores
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        alert("Yay! Scissors beats Paper!")
+        //increment human score. show scores
+    }
+    else {
+        alert("It's a draw!")
+        //just show scores
+    }
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+
+}
