fa7e671f7b992be598d878d778e0f1cb82ea1dcb
Bahar Abdi <bahar.abdi04@gmail.com>
2024-06-25 17:10:25 -0500

    Include if statements for humanChoice in case user quits/cancels game early, while also adding more semicolons where it's needed

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function getComputerChoice() {
     let computerChoice;
     switch (randomVal) {
         case 0:
-            computerChoice = 'rock'
+            computerChoice = 'rock';
             break;
         case 1:
-            computerChoice = 'paper'
+            computerChoice = 'paper';
             break;
         case 2:
-            computerChoice = 'scissors'
+            computerChoice = 'scissors';
             break;
         default:
             console.log("Must be equal to one out of the three choices");
@@ -19,6 +19,10 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Let's play a quick fun game! Write down one of the following choices below:\n'Rock', 'Paper', 'Scissors'");
+    if (choice === null) {
+        alert("Cancelled/Exited Game");
+        return null;
+    }
     return choice.toLowerCase();
 }
 
@@ -27,37 +31,40 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'paper') {
-        computerScore++
+        computerScore++;
         alert(`Oh no, you lost this round... Paper covers Rock\nComputer: ${computerScore} You: ${humanScore}`);
     }
     else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        humanScore++
-        alert(`Yay! Rock beats Scissors!\nComputer: ${computerScore} You: ${humanScore}`)
+        humanScore++;
+        alert(`Yay! Rock beats Scissors!\nComputer: ${computerScore} You: ${humanScore}`);
     }
     else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        humanScore++
-        alert(`Yay! Paper beats Rock!\nComputer: ${computerScore} You: ${humanScore}`)
+        humanScore++;
+        alert(`Yay! Paper beats Rock!\nComputer: ${computerScore} You: ${humanScore}`);
     }
     else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        computerScore++
-        alert(`Oh no, you lost this round... Scissors cut paper\nComputer: ${computerScore} You: ${humanScore}`)
+        computerScore++;
+        alert(`Oh no, you lost this round... Scissors cut paper\nComputer: ${computerScore} You: ${humanScore}`);
     }
     else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        computerScore++
-        alert(`Oh no, you lost this round... Rock crushes Scissors\nComputer: ${computerScore} You: ${humanScore}`)
+        computerScore++;
+        alert(`Oh no, you lost this round... Rock crushes Scissors\nComputer: ${computerScore} You: ${humanScore}`);
     }
     else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        humanScore++
-        alert(`Yay! Scissors beats Paper!\nComputer: ${computerScore} You: ${humanScore}`)
+        humanScore++;
+        alert(`Yay! Scissors beats Paper!\nComputer: ${computerScore} You: ${humanScore}`);
     }
     else {
-        alert(`It's a draw!\nComputer: ${computerScore} You: ${humanScore}`)
+        alert(`It's a draw!\nComputer: ${computerScore} You: ${humanScore}`);
     }
 }
 
 function playGame() {
     for (let i=0; i<5; i++) {
         const humanSelection = getHumanChoice();
+        if (humanSelection === null) {
+            return;
+        }
         const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
     }
@@ -66,10 +73,10 @@ function playGame() {
         alert(`You Win! With a score of ${humanScore} against ${computerScore}`);
     }
     else if (humanScore < computerScore) {
-        alert(`The computer won with a score of ${computerScore} against your ${humanScore}. Better luck next time.`)
+        alert(`The computer won with a score of ${computerScore} against your ${humanScore}. Better luck next time.`);
     }
     else {
-        alert(`How crazy is it that you ended with a tie of ${humanScore} : ${computerScore}?!`)
+        alert(`How crazy is it that you ended with a tie of ${humanScore} : ${computerScore}?!`);
     }
 }
 playGame();
